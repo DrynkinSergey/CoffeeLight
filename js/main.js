f7b2c41aab99b3bcc9f364d453abdcf42c895baa
Sergey <sergsmile29@gmail.com>
2023-02-04 13:42:26 +0200

    animation was finished

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
   slidesPerView: 1.5,
@@ -14,47 +13,51 @@ const swiper = new Swiper('.swiper', {
 
 });
 
-gsap.from(
+
+
+let tlHeader = gsap.timeline();
+tlHeader.from(
   '.title', {
   xPercent: -100,
   duration: 1
-})
-gsap.from(
-  '.block-on-cup', {
-  y: -100,
-  opacity: 0,
-  duration: 1,
-  delay: 1.4
-})
-gsap.from(
-  '#cup', {
-  yPercent: 100,
-  opacity: 0,
-  duration: 1,
-})
-gsap.from(
+}, .4)
+  .from(
+    '.block-on-cup', {
+    y: -100,
+    opacity: 0,
+    duration: .5,
+  })
+  .from(
+    '#cup', {
+    yPercent: 100,
+    opacity: 0,
+    duration: .4,
+  })
+  .from(
 
-  '.btn-header', {
-  yPercent: 100,
-  opacity: 0,
-  duration: 1,
-  delay: 1.5
-})
-gsap.from(
-  '.a-header', {
-  y: 70,
-  opacity: 0,
-  duration: 2,
-  delay: 1.7
-})
-gsap.from(
-  '.h2-header', {
-  y: 100,
-  opacity: 0,
-  duration: .9,
-  delay: 1.3
-})
-
+    '.btn-header', {
+    yPercent: 100,
+    opacity: 0,
+    duration: 1,
+  })
+  .from(
+    '.a-header', {
+    y: 70,
+    opacity: 0,
+    duration: 1,
+  }, .4)
+  .from(
+    '.h2-header', {
+    y: 100,
+    opacity: 0,
+    duration: .9,
+  }, .8)
+const tlMessageBtn = gsap.timeline({ defaults: { duration: .2 }, repeat: -1, repeatDelay: 3 });
+tlMessageBtn
+  .to('#cup', { rotate: 5 })
+  .to('#cup', { rotate: -5 })
+  .to('#cup', { rotate: 5 })
+  .to('#cup', { rotate: 0 });
 //scroll
 let tl = gsap.timeline();
 
@@ -83,9 +86,10 @@ tl.from('.h1-explore', {
       start: 'top center',
       end: '+=750',
       scrub: 2,
-      stagger: 0.3,
+
     },
-    yPercent: 200
+    yPercent: 200,
+    stagger: 0.3,
   })
   .from('#explore-us-section', {
     scrollTrigger: {
@@ -115,3 +119,60 @@ tl.from('.h1-explore', {
     yPercent: 50,
     opacity: 0,
   })
+  .from('#order-now-btn', {
+    scrollTrigger: {
+      trigger: '#order-coffee-content',
+      start: 'top center',
+      end: '+=100',
+      scrub: 1,
+    },
+    y: 50,
+    opacity: 0,
+  })
+  .from('#best-selling-title', {
+    scrollTrigger: {
+      trigger: '#best-selling',
+      start: '-200px center',
+      end: '+=150',
+      scrub: 1,
+    },
+    y: 150,
+    opacity: 0,
+  })
+  .from('#best-selling-subtitle', {
+    scrollTrigger: {
+
+      trigger: '#best-selling',
+      start: '-100px center',
+      end: '+=250',
+      scrub: 2,
+    },
+    y: 120,
+    opacity: 0,
+  })
+  .from('#card-explore-best-sell', {
+    scrollTrigger: {
+
+      trigger: '#best-selling',
+      start: '20% center',
+      end: '+=200',
+      scrub: 2,
+    },
+    opacity: 0,
+    scale: 0,
+    stagger: .3
+  })
+  .from('#mobile-app-text', {
+    scrollTrigger: {
+
+      trigger: '#mobile-app',
+      start: '-200px center',
+      end: '+=300',
+      scrub: 1,
+    },
+    opacity: 0,
+    y: 150,
+    stagger: .1
+  })
+
+
